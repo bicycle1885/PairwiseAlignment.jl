5074cb513cfcd404434698398e08364bb2586aed
Kenta Sato <bicycle1885@gmail.com>
2015-09-24 21:16:38 +0900

    affine gap

--- src/PairwiseAlignment.jl
+++ src/PairwiseAlignment.jl
@@ -1,5 +1,20 @@
 module PairwiseAlignment
 
-# package code goes here
+export
+    pairalign,
+    # scoring systems
+    AffineGap,
+    # alignment types
+    GlobalAlignment,
+    LocalAlignment
+
+# alignment types
+immutable GlobalAlignment end
+immutable LocalAlignment end
+immutable SemiGlobalAlignment end
+
+include("align.jl")
+include("result.jl")
+include("global_affine_gap.jl")
 
 end # module
