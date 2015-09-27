6cb8af242eea45d107b5d04ef46b91e1daaef326
Kenta Sato <bicycle1885@gmail.com>
2015-09-28 06:24:34 +0900

    clean up

--- src/algorithm/affinegap_global_align.jl
+++ src/algorithm/affinegap_global_align.jl
@@ -52,18 +52,18 @@ function traceback{T}(a, b, H, E, F, subst_matrix::AbstractMatrix{T}, gap_open_p
     i = length(a)
     j = length(b)
     while i ≥ 1 || j ≥ 1
-        if i ≥ 1 && j ≥ 1 && H[i+1,j+1] - subst_matrix[a[i],b[j]] == H[i,j]
+        if i ≥ 1 && j ≥ 1 && H[i+1,j+1] == H[i,j] + subst_matrix[a[i],b[j]]
             # ↖
             push!(a′, a[i])
             push!(b′, b[j])
             i -= 1
             j -= 1
-        elseif i == 0 || (j ≥ 1 && H[i+1,j+1] == E[i,j] && ((j ≥ 2 && E[i,j] + ge == E[i,j-1]) || E[i,j] + goe == H[i+1,j]))
+        elseif i == 0 || (j ≥ 1 && H[i+1,j+1] == E[i,j] && ((j ≥ 2 && E[i,j] == E[i,j-1] - ge) || E[i,j] == H[i+1,j] - goe))
             # ←
             push!(a′, '-')
             push!(b′, b[j])
             j -= 1
-        elseif j == 0 || (i ≥ 1 && H[i+1,j+1] == F[i,j] && ((i ≥ 2 && F[i,j] + ge == F[i-1,j]) || F[i,j] + goe == H[i,j+1]))
+        elseif j == 0 || (i ≥ 1 && H[i+1,j+1] == F[i,j] && ((i ≥ 2 && F[i,j] == F[i-1,j] - ge) || F[i,j] == H[i,j+1] - goe))
             # ↑
             push!(a′, a[i])
             push!(b′, '-')
--- src/algorithm/affinegap_local_align.jl
+++ src/algorithm/affinegap_local_align.jl
@@ -57,18 +57,18 @@ function traceback{T}(a, b, H, E, F, best_endpos, subst_matrix::AbstractMatrix{T
     b′ = Char[]
     i, j = best_endpos
     while H[i,j] > 0
-        if i ≥ 1 && j ≥ 1 && H[i+1,j+1] - subst_matrix[a[i],b[j]] == H[i,j]
+        if i ≥ 1 && j ≥ 1 && H[i+1,j+1] == H[i,j] + subst_matrix[a[i],b[j]]
             # ↖
             push!(a′, a[i])
             push!(b′, b[j])
             i -= 1
             j -= 1
-        elseif i == 0 || (j ≥ 1 && H[i+1,j+1] == E[i,j] && ((j ≥ 2 && E[i,j] + ge == E[i,j-1]) || E[i,j] + goe == H[i+1,j]))
+        elseif i == 0 || (j ≥ 1 && H[i+1,j+1] == E[i,j] && ((j ≥ 2 && E[i,j] == E[i,j-1] - ge) || E[i,j] == H[i+1,j] - goe))
             # ←
             push!(a′, '-')
             push!(b′, b[j])
             j -= 1
-        elseif j == 0 || (i ≥ 1 && H[i+1,j+1] == F[i,j] && ((i ≥ 2 && F[i,j] + ge == F[i-1,j]) || F[i,j] + goe == H[i,j+1]))
+        elseif j == 0 || (i ≥ 1 && H[i+1,j+1] == F[i,j] && ((i ≥ 2 && F[i,j] == F[i-1,j] - ge) || F[i,j] == H[i,j+1] - goe))
             # ↑
             push!(a′, a[i])
             push!(b′, '-')
